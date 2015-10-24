0372261c105d13c0049d5618fb3f19809c8eeacd
deepali_futane <deepali.futane@veritas.com>
2015-10-25 00:35:23 +0530

    API Integration Changes

--- Vision/www/js/index.js
+++ Vision/www/js/index.js
@@ -41,26 +41,11 @@ var app = {
                         alert('success');
                         console.log(JSON.stringify(data));
                         $.each(data, function (i, theItem) {
-                            /*var combo = document.getElementById("cboFastBikes");
-                            var option = document.createElement("option");
-                            option.text = theItem.toString();
-                            option.value = theItem.toString();
-                            try {
-                                //alert('success add combo');
-                                combo.add(option, null); // Other browsers
-                            }
-                            catch (error) {
-                                alert('error found');
-                                combo.add(option); // really old browser
-                            }*/
                         	 document.getElementById("inpFirstName").value= theItem.login;
-
                         });
                     },
                     error: function (msg, url, line) {
-                        //console.log('error trapped in error: function('+msg+ url+ line+')');
                         console.log('msg = ' + msg + ', url = ' + url + ', line = ' + line);
-                       // console.log("Error");
                     }
                     
                 });
@@ -69,24 +54,69 @@ var app = {
       	  alert( "Handler for .submit() called." );
       	  var firstName = document.getElementById("inpFirstName").value;
       	  var lastName = document.getElementById("inpLastName").value;
-      	   $.ajax(
-                 {
-                     type: "POST",
-                     url: 'http://vcslx160.samgpunb.symantec.com:8000/api/login/?login=deepali2&password=symc',
-                     dataType: "json",
-                     success: function (data) {
-                    	 	alert('success');
-                     },
-                     error: function (msg, url, line) {
-                         //console.log('error trapped in error: function('+msg+ url+ line+')');
-                         console.log('msg = ' + msg + ', url = ' + url + ', line = ' + line);
-                        // console.log("Error");
-                     }
-                     
-                 });
+       	  
+      	 $.ajax({
+             type: "POST",
+             url: "http://vcslx160.samgpunb.symantec.com:8000/api/login",
+             async: false,
+             data: JSON.stringify({ "login":"sup", "password":"symc" }),
+             contentType: "application/json",
+             complete: function (data) {
+             console.log(data);
+             wait = false;
+         }});
       	  event.preventDefault();
       	  
       	});
+        
+        $("#register_form" ).submit(function( event ) {
+            alert( "Handler for .submit() called." );
+            
+            var firstName		= document.getElementById("first_name").value;
+            var lastName 		= document.getElementById("last_name").value;
+            var password 		= document.getElementById("password").value;
+            var reenterPassword = document.getElementById("reenter_password").value;
+            var gender			= document.getElementById("gender").value;
+            var dateOfBirth 	= document.getElementById("dob").value;
+            var mobile 			= document.getElementById("mobile").value;
+            var email 			= document.getElementById("email").value;
+            var occupation 		= document.getElementById("occupation").value;
+            var state 			= document.getElementById("state").value;
+            var city 			= document.getElementById("city").value;
+            var pincode 		= document.getElementById("pincode").value;
+            var address 		= document.getElementById("address").value;
+            
+            var payload = {
+                    "address": address,
+                    "pincode": pincode,
+                    "city": city,
+                    "state": state,
+                    "email": email,
+                    "mobile": mobile,
+                    "dob": dateOfBirth,
+                    "gender": gender,
+                    "lastname": lastName,
+                    "firstname": firstName
+            }
+            console.log("registration payload");
+            console.log(JSON.stringify(payload));
+            
+            $.ajax({
+                type: "POST",
+                url: "http://vcslx160.samgpunb.symantec.com:8000/api/register/",
+                async: false,
+                data: JSON.stringify({"address":"3","pincode":"2","city":"mum","state":"mah","email":"d","mobile":"34","dob":"03/09/1990","gender":"on","lastname":"Futane","firstname":"Deepali"}),
+                //data:JSON.stringify(payload),
+                contentType: "application/json",
+                complete: function (data) {
+                    console.log(JSON.stringify(data));
+                    wait = false;
+            }});
+            event.preventDefault();
+            
+           });
+
+
     }
    
 };
